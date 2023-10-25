b8fa8a103eaaa76e4c4e3cb06c9f51d9ef1c8d49
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-10-26 00:40:26 +0300

    2

--- admin/update-teacher-pic.php
+++ admin/update-teacher-pic.php
@@ -7,3 +7,26 @@ if(strlen($_SESSION['aid'])==0)
 header('location:index.php');
 }
 else{
+    if(isset($_POST['submit'])){
+
+        $lid=intval($_GET['tid']); 
+        //Getting Post Values  
+        $currentpic=$_POST['currentprofilepic'];
+        $oldprofilepic="teacherspic"."/".$currentpic;
+        $profilepic=$_FILES["profilepic"]["name"];
+        // get the image extension
+        $extension = substr($profilepic,strlen($profilepic)-4,strlen($profilepic));
+        // allowed extensions
+        $allowed_extensions = array(".jpg","jpeg",".png",".gif");
+        // Validation for allowed extensions .in_array() function searches an array for a specific value.
+        if(!in_array($extension,$allowed_extensions))
+        {
+        echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+        }
+        else
+        {
+        //rename the image file
+        $newprofilepic=md5($profilepic).time().$extension;
+        // Code for move image into directory
+        move_uploaded_file($_FILES["profilepic"]["tmp_name"],"teacherspic/".$newprofilepic);
+        
