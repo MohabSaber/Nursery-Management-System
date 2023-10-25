90aa4e85281dd82a5983521767c524b0cf2a4c1d
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-10-26 00:07:18 +0300

    edit classes

--- classes.php
+++ classes.php
@@ -9,26 +9,24 @@
     <meta content="" name="keywords">
     <meta content="" name="description">
 
-    <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
 
-    <!-- Google Web Fonts -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet">
     
-    <!-- Icon Font Stylesheet -->
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
+
     <link href="lib/animate/animate.min.css" rel="stylesheet">
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-    <!-- Customized Bootstrap Stylesheet -->
+
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
 </head>
 
@@ -36,7 +34,6 @@
  <?php include_once('includes/header.php');?>
 
 
-        <!-- Page Header End -->
         <div class="container-xxl py-5 page-header position-relative mb-5">
             <div class="container py-5">
                 <h1 class="display-2 text-white animated slideInDown mb-4">Classes</h1>
@@ -49,10 +46,7 @@
                 </nav>
             </div>
         </div>
-        <!-- Page Header End -->
 
-
-        <!-- Classes Start -->
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
@@ -107,10 +101,7 @@ while($result=mysqli_fetch_array($query)){
                 </div>
             </div>
         </div>
-        <!-- Classes End -->
 
-
-        <!-- Appointment Start -->
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="bg-light rounded">
@@ -186,22 +177,15 @@ while($result=mysqli_fetch_array($query)){
                 </div>
             </div>
         </div>
-        <!-- Appointment End -->
 
 
-              <!-- Testimonial End -->
 
-
-        <!-- Footer Start -->
          <?php include_once('includes/footer.php');?>
-        <!-- Footer End -->
 
-
-        <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
 
-    <!-- JavaScript Libraries -->
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/wow/wow.min.js"></script>
@@ -209,7 +193,7 @@ while($result=mysqli_fetch_array($query)){
     <script src="lib/waypoints/waypoints.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-    <!-- Template Javascript -->
+
     <script src="js/main.js"></script>
 </body>
 
