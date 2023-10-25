0acbe78bbd10774f5cc4c0ac5fe88aba3b7e5413
Mohab Saber <125674416+MohabSaber@users.noreply.github.com>
2023-10-26 02:07:46 +0300

    Update aboutus.php

--- admin/aboutus.php
+++ admin/aboutus.php
@@ -122,4 +122,25 @@ while ($row=mysqli_fetch_array($ret)) {
 
 </div>
 
+  <!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+<script src="nic.js"></script>
+<!-- Page specific script -->
+<script>
+$(function () {
+  bsCustomFileInput.init();
+});
+</script>
+</body>
+</html>
+<?php } ?>
+
 
