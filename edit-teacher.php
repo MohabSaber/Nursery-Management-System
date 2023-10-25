895420abf6f35f5649eac62a9948586c41e8e440
Omar Abdelazim <125689089+OmarAzima@users.noreply.github.com>
2023-10-26 01:29:46 +0300

    Update edit-teacher.php

--- edit-teacher.php
+++ edit-teacher.php
@@ -1,15 +1,15 @@
 <?php session_start();
-// Database Connection
+
 include('includes/config.php');
-//Validating Session
+
 if(strlen($_SESSION['aid'])==0)
   { 
 header('location:index.php');
 }
 else{
-// Code for update teacher details
+
 if(isset($_POST['submit'])){
-//Getting Post Values  
+
 $fname=$_POST['fullname'];
 $email=$_POST['emailid'];
 $mobileno=$_POST['mobilenumber'];
@@ -39,30 +39,30 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
 
   <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- Select2 -->
+  
   <link rel="stylesheet" href="../plugins/select2/css/select2.min.css">
   <link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-  <!-- Bootstrap4 Duallistbox -->
+  
   <link rel="stylesheet" href="../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
-  <!-- BS Stepper -->
+ 
   <link rel="stylesheet" href="../plugins/bs-stepper/css/bs-stepper.min.css">
-  <!-- dropzonejs -->
+
   <link rel="stylesheet" href="../plugins/dropzone/min/dropzone.min.css">
-  <!-- Theme style -->
+ 
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-  <!-- Navbar -->
+ 
 <?php include_once("includes/navbar.php");?>
-  <!-- /.navbar -->
+ 
 
-  <!-- Main Sidebar Container -->
+
  <?php include_once("includes/sidebar.php");?>
 
-  <!-- Content Wrapper. Contains page content -->
+  
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+   
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -76,16 +76,16 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
             </ol>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
+      </div>
     </section>
 
-    <!-- Main content -->
+   
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <!-- left column -->
+         
           <div class="col-md-8">
-            <!-- general form elements -->
+           
             <div class="card card-primary">
 
 <?php
@@ -99,29 +99,28 @@ while($result=mysqli_fetch_array($query)){
               <div class="card-header">
                 <h3 class="card-title">Persoanl Info</h3>
               </div>
-              <!-- /.card-header -->
-              <!-- form start -->
+             
               <form name="addlawyer" method="post" enctype="multipart/form-data">
                 <div class="card-body">
 
-<!--  Full Name--->
+
    <div class="form-group">
                     <label for="exampleInputFullname">Full Name</label>
                     <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Enter Teacher Full Name" value="<?php echo $result['fullName']?>" required>
                   </div>
-<!--   Email---->
+
                   <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" class="form-control" id="emailid" name="emailid" value="<?php echo $result['teacherEmail']?>" placeholder="Enter email" required>
                   </div>
-<!--Number---->
+
                   <div class="form-group">
                     <label for="text">Mobile Number</label>
                     <input type="text" class="form-control" id="mobilenumber" name="mobilenumber" placeholder="Enter email" pattern="[0-9]{10}" title="10 numeric characters only" value="<?php echo $result['teacherMobileNo']?>" required>
                   </div>
 
 
-<!--Subject---->
+
                   <div class="form-group">
                     <label for="text">Subject/Designation</label>
                     <input type="text" class="form-control" id="tsubject" value="<?php echo $result['teacherSubject']?>" name="tsubject" placeholder="Enter Subject/Designation"  required>
@@ -129,7 +128,7 @@ while($result=mysqli_fetch_array($query)){
 
 
 
-  <!--Profile Pic---->
+  
   <div class="form-group">
                     <label for="exampleInputFile">Profile Pic </label>
                <img src="teacherspic/<?php echo $result['teacherPic']?>" width="120">
@@ -160,38 +159,38 @@ while($result=mysqli_fetch_array($query)){
        
   
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+      
+      </div>
     </section>
-    <!-- /.content -->
+  
   </div>
-  <!-- /.content-wrapper -->
+
 <?php include_once('includes/footer.php');?>
 
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery -->
+
+
 <script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
+
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- bs-custom-file-input -->
+
 <script src="../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-<!-- AdminLTE App -->
+
 <script src="../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="../dist/js/demo.js"></script>
-<!-- Page specific script -->
+
 <script src="../plugins/select2/js/select2.full.min.js"></script>
 <script>
 $(function () {
   bsCustomFileInput.init();
 });
   $(function () {
-    //Initialize Select2 Elements
+ 
     $('.select2').select2()
 
-    //Initialize Select2 Elements
+    
     $('.select2bs4').select2({
       theme: 'bootstrap4'
     })
