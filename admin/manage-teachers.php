c83d164fdeb5845e4592f5d9e338c45130b99f95
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-10-26 00:32:14 +0300

    final

--- admin/manage-teachers.php
+++ admin/manage-teachers.php
@@ -1,14 +1,14 @@
 <?php session_start();
 error_reporting(0);
-// Database Connection
+
 include('includes/config.php');
-//Validating Session
+
 if(strlen($_SESSION['aid'])==0)
   { 
 header('location:index.php');
 }
 else{
-//Code For Deletion the teacher
+
 if($_GET['action']=='delete'){
 $lid=intval($_GET['tid']);
 $profilepic=$_GET['profilepic'];
@@ -26,35 +26,29 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
 
 
   ?>
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PreSchool Enrollment System  | Manage Teachers</title>
 
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-  <!-- DataTables -->
   <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-  <!-- Navbar -->
 <?php include_once("includes/navbar.php");?>
-  <!-- /.navbar -->
 
  <?php include_once("includes/sidebar.php");?>
 
-  <!-- Content Wrapper. Contains page content -->
+
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -68,9 +62,9 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
             </ol>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
+      </div>
     </section>
-    <!-- Main content -->
+
     <section class="content">
       <div class="container-fluid">
         <div class="row">
@@ -82,7 +76,6 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
               <div class="card-header">
                 <h3 class="card-title">teachersDetails</h3>
               </div>
-              <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
                   <thead>
@@ -102,7 +95,8 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
 $cnt=1;
 while($result=mysqli_fetch_array($query)){
 ?>
-   <tr>
+
+                  <tr>
                     <td><?php echo $cnt;?></td>
                     <td><img src="teacherspic/<?php echo $result['teacherPic']?>" width="80"></td>
                     <td><?php echo $result['fullName']?></td>
@@ -132,34 +126,26 @@ while($result=mysqli_fetch_array($query)){
                   </tfoot>
                 </table>
               </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
       </div>
-      <!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
+
 <?php include_once('includes/footer.php');?>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
 
-<!-- jQuery -->
+  <aside class="control-sidebar control-sidebar-dark">
+
+  </aside>
+
+</div>
+
 <script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
+
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
+
 <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
@@ -172,11 +158,11 @@ while($result=mysqli_fetch_array($query)){
 <script src="../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="../plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- AdminLTE App -->
+
 <script src="../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="../dist/js/demo.js"></script>
-<!-- Page specific script -->
+
 <script>
   $(function () {
     $("#example1").DataTable({
