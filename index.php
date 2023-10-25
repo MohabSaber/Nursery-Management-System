063bed2e0774e612f02f1d24e1102c1c52cac477
Omar Abdelazim <125689089+OmarAzima@users.noreply.github.com>
2023-10-26 01:31:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,13 +26,12 @@ if(isset($_POST['login']))
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PreSchool Enrollment System | Login</title>
 
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
+ 
   <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
+ 
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
@@ -66,11 +65,11 @@ if(isset($_POST['login']))
           <div class="col-8">
    
           </div>
-          <!-- /.col -->
+          
           <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block" name="login">Sign In</button>
           </div>
-          <!-- /.col -->
+        
         </div>
       </form>
 
@@ -80,17 +79,17 @@ if(isset($_POST['login']))
       </p>
 
     </div>
-    <!-- /.card-body -->
+   
   </div>
-  <!-- /.card -->
+  
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery -->
+
+
 <script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
+
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
+
 <script src="../dist/js/adminlte.min.js"></script>
 </body>
 </html>
