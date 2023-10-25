961c4ddf9f364fbb4d57049a948f2306964f017e
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-10-26 00:08:49 +0300

    edit visit

--- visit.php
+++ visit.php
@@ -28,23 +28,23 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-    <!-- Google Web Fonts -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@600&family=Lobster+Two:wght@700&display=swap" rel="stylesheet">
     
-    <!-- Icon Font Stylesheet -->
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
+
     <link href="lib/animate/animate.min.css" rel="stylesheet">
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-    <!-- Customized Bootstrap Stylesheet -->
+ 
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
+
     <link href="css/style.css" rel="stylesheet">
 </head>
 
@@ -52,7 +52,7 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
  <?php include_once('includes/header.php');?>
 
 
-        <!-- Page Header End -->
+
         <div class="container-xxl py-5 page-header position-relative mb-5">
             <div class="container py-5">
                 <h1 class="display-2 text-white animated slideInDown mb-4">Schedule a Visit</h1>
@@ -65,10 +65,9 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
                 </nav>
             </div>
         </div>
-        <!-- Page Header End -->
 
 
-        <!-- Appointment Start -->
+
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="bg-light rounded">
@@ -144,19 +143,19 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
                 </div>
             </div>
         </div>
-        <!-- Appointment End -->
 
 
-        <!-- Footer Start -->
+
+
             <?php include_once('includes/footer.php');?>       
-             <!-- Footer End -->
 
 
-        <!-- Back to Top -->
+
+
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
 
-    <!-- JavaScript Libraries -->
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/wow/wow.min.js"></script>
@@ -164,7 +163,7 @@ echo "<script>alert('Something went wrong. Please try again.');</script>";
     <script src="lib/waypoints/waypoints.min.js"></script>
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-    <!-- Template Javascript -->
+
     <script src="js/main.js"></script>
 </body>
 
