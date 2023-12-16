fd9d81f0d2421aa1875469d23fa8d33f9ede04fa
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-12-16 20:58:35 +0200

    edit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,7 @@
-?php session_start();
-
+<?php session_start();
+// Database Connection
 include('includes/config.php');
-
+//Validating Session
 if(strlen($_SESSION['aid'])==0)
   { 
 header('location:index.php');
