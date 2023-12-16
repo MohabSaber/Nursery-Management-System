77ff240f93d0c6a006fcd14004a9bec3d8e7d1dc
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2023-12-16 20:42:46 +0200

    update add teacher

--- admin/add-teacher.php
+++ admin/add-teacher.php
@@ -215,7 +215,11 @@ $(function () {
     //Initialize Select2 Elements
     $('.select2').select2()
 
-
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+});
 </script>
 </body>
 </html>
