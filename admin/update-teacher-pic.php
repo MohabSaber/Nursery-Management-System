10261c8b41cf6a4211046369ec6b774067d3be2b
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-12-19 20:19:43 +0300

    add update teacher picture

--- admin/update-teacher-pic.php
+++ admin/update-teacher-pic.php
@@ -1,49 +1,50 @@
-<?php session_start();
-
+<?php
+session_start();
 include('includes/config.php');
 
-if(strlen($_SESSION['aid'])==0)
-  { 
-header('location:index.php');
-}
-else{
+class UpdateTeacherPic {
+    private $con;
 
-if(isset($_POST['submit'])){
+    public function __construct($con) {
+        $this->con = $con;
+    }
 
-$lid=intval($_GET['tid']); 
-  
-$currentpic=$_POST['currentprofilepic'];
-$oldprofilepic="teacherspic"."/".$currentpic;
-$profilepic=$_FILES["profilepic"]["name"];
+    public function updateTeacherPicture() {
+        if (strlen($_SESSION['aid']) == 0) {
+            header('location:index.php');
+        } else {
+            if (isset($_POST['submit'])) {
+                $lid = intval($_GET['tid']);
+                $currentpic = $_POST['currentprofilepic'];
+                $oldprofilepic = "teacherspic" . "/" . $currentpic;
+                $profilepic = $_FILES["profilepic"]["name"];
+                $extension = substr($profilepic, strlen($profilepic) - 4, strlen($profilepic));
+                $allowed_extensions = array(".jpg", "jpeg", ".png", ".gif");
 
-$extension = substr($profilepic,strlen($profilepic)-4,strlen($profilepic));
+                if (!in_array($extension, $allowed_extensions)) {
+                    echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+                } else {
+                    $newprofilepic = md5($profilepic) . time() . $extension;
+                    move_uploaded_file($_FILES["profilepic"]["tmp_name"], "teacherspic/" . $newprofilepic);
 
-$allowed_extensions = array(".jpg","jpeg",".png",".gif");
-if(!in_array($extension,$allowed_extensions))
-{
-echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
-}
-else
-{
-$newprofilepic=md5($profilepic).time().$extension;
-move_uploaded_file($_FILES["profilepic"]["tmp_name"],"teacherspic/".$newprofilepic);
-
-
-
-
-
-$query=mysqli_query($con,"update tblteachers set teacherPic='$newprofilepic' where id='$lid'");
-if($query){
-unlink($oldprofilepic);  
-echo "<script>alert('Teacher pic updated successfully.');</script>";
-echo "<script type='text/javascript'> document.location = 'manage-teachers.php'; </script>";
-} else {
-echo "<script>alert('Something went wron. Please try again.');</script>";
-}
-}
+                    $query = mysqli_query($this->con, "update tblteachers set teacherPic='$newprofilepic' where id='$lid'");
+                    if ($query) {
+                        unlink($oldprofilepic);
+                        echo "<script>alert('Teacher pic updated successfully.');</script>";
+                        echo "<script type='text/javascript'> document.location = 'manage-teachers.php'; </script>";
+                    } else {
+                        echo "<script>alert('Something went wrong. Please try again.');</script>";
+                    }
+                }
+            }
+        }
+    }
 }
 
-  ?>
+$updateTeacherPic = new UpdateTeacherPic($con);
+$updateTeacherPic->updateTeacherPicture();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,23 +55,30 @@ echo "<script>alert('Something went wron. Please try again.');</script>";
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
 
   <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-
+  <!-- Select2 -->
   <link rel="stylesheet" href="../plugins/select2/css/select2.min.css">
   <link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- Bootstrap4 Duallistbox -->
   <link rel="stylesheet" href="../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+  <!-- BS Stepper -->
   <link rel="stylesheet" href="../plugins/bs-stepper/css/bs-stepper.min.css">
+  <!-- dropzonejs -->
   <link rel="stylesheet" href="../plugins/dropzone/min/dropzone.min.css">
+  <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-
+  <!-- Navbar -->
 <?php include_once("includes/navbar.php");?>
+  <!-- /.navbar -->
 
-
+  <!-- Main Sidebar Container -->
  <?php include_once("includes/sidebar.php");?>
 
+  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -84,19 +92,22 @@ echo "<script>alert('Something went wron. Please try again.');</script>";
             </ol>
           </div>
         </div>
-      </div>
+      </div><!-- /.container-fluid -->
     </section>
 
+    <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
+          <!-- left column -->
           <div class="col-md-8">
-   
+            <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">Profile Picture</h3>
               </div>
-
+              <!-- /.card-header -->
+              <!-- form start -->
               <form name="addlawyer" method="post" enctype="multipart/form-data">
                 <div class="card-body">
 
@@ -108,14 +119,14 @@ while($result=mysqli_fetch_array($query))
 ?>
 
 
-
+<!--  Current Profile Pic--->
    <div class="form-group">
                     <label for="exampleInputFullname">Current Profile Pic</label>
        <img src="teacherspic/<?php echo $result['teacherPic']?>" width="200">
                   </div>
 
 <?php } ?>
-
+  <!--Profile Pic---->
   <div class="form-group">
                     <label for="exampleInputFile">New Profile Pic <span style="font-size:12px;color:red;">(Only jpg / jpeg/ png /gif format allowed)</span></label>
                     <div class="input-group">
@@ -138,39 +149,49 @@ while($result=mysqli_fetch_array($query))
 
 
                 </div>
-  
+                <!-- /.card-body -->
           
             </div>
-
+            <!-- /.card -->
           </div>
-
+          <!--/.col (left) -->
 
 
 
   
         </div>
-
-      </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
     </section>
+    <!-- /.content -->
   </div>
-
+  <!-- /.content-wrapper -->
 <?php include_once('includes/footer.php');?>
 
 </div>
+<!-- ./wrapper -->
 
+<!-- jQuery -->
 <script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- bs-custom-file-input -->
 <script src="../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<!-- AdminLTE App -->
 <script src="../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
+<!-- Page specific script -->
 <script src="../plugins/select2/js/select2.full.min.js"></script>
 <script>
 $(function () {
   bsCustomFileInput.init();
 });
   $(function () {
+    //Initialize Select2 Elements
     $('.select2').select2()
 
+    //Initialize Select2 Elements
     $('.select2bs4').select2({
       theme: 'bootstrap4'
     })
@@ -178,4 +199,4 @@ $(function () {
 </script>
 </body>
 </html>
-<?php } ?>
+<?php ?>
