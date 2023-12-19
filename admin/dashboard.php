a10c6623f9a5e5cb8ac6e943dc571495c64aeb12
Mohab Saber <125674416+MohabSaber@users.noreply.github.com>
2023-12-19 15:05:58 +0200

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -39,84 +39,191 @@ else{ ?>
 <div class="wrapper">
 
 
-
+  <!-- Navbar -->
 <?php include_once('includes/navbar.php');?>
 
-
+  <!-- Main Sidebar Container -->
 
 <?php include_once('includes/sidebar.php');?>
+    <!-- Sidebar -->
 
 
-
-
+  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-
+    <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard</h1>
-          </div>
+          </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
               <li class="breadcrumb-item active">Dashboard</li>
             </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+
+          <?php if($_SESSION['utype']==1):?>
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+<?php $query=mysqli_query($con,"select id from tbladmin where UserType=0");
+$subadmincount=mysqli_num_rows($query);
+?>
+
+
+                <h3><?php echo $subadmincount;?></h3>
+                <p>Sub Admins</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person"></i>
+              </div>
+              <a href="manage-subadmins.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        <?php endif;?>
+          <!-- ./col -->
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+<?php $query1=mysqli_query($con,"select id from tblteachers");
+$listedteachers=mysqli_num_rows($query1);
+?>
+
+                <h3><?php echo $listedteachers;?></h3>
+
+                <p>Listed Teachers</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="manage-teachers.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+ <?php $query3=mysqli_query($con,"select id from tblclasses");
+$listedclasses=mysqli_num_rows($query3);
+?>               
+                <h3><?php echo $listedclasses;?></h3>
+
+                <p>Listed Classes</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="manage-classes.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
 
 
-    </div>
-   
-      </div>
-    </section>
+<hr />
+
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+<?php $query11=mysqli_query($con,"select id from tblenrollment ");
+$terollments=mysqli_num_rows($query11);
+?>
+
+
+                <h3><?php echo $terollments;?></h3>
+                <p>Total Enrollments</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person"></i>
+              </div>
+              <a href="all-enrolments.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
  
-  </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+<?php $query12=mysqli_query($con,"select id from tblenrollment where (enrollStatus='' || enrollStatus is null)");
+$newerollments=mysqli_num_rows($query12);
+?>
 
-<?php include_once('includes/footer.php');?>
+                <h3><?php echo $newerollments;?></h3>
+
+                <p>New Enrollments</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="new-enrollments.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+ <?php $query13=mysqli_query($con,"select id from tblenrollment where (enrollStatus='Accepted')");
+$acceptedenrollments=mysqli_num_rows($query13);
+?>               
+                <h3><?php echo $acceptedenrollments;?></h3>
+
+                <p>Accepted Classes</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="accepted-enrolment.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+       <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+ <?php $query14=mysqli_query($con,"select id from tblenrollment where (enrollStatus='Rejected')");
+$rejectedenrolmments=mysqli_num_rows($query14);
+?>               
+                <h3><?php echo $rejectedenrolmments;?></h3>
+
+                <p>Rejected Classes</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="rejected-enrolments.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
 
 
-</div>
-
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="../plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="../plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="../plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="../plugins/moment/moment.min.js"></script>
-<script src="../plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="../plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../dist/js/pages/dashboard.js"></script>
-</body>
-</html>
-<?php } ?>
-
-   
 
 
+
+
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+              <div class="inner">
+<?php $query111=mysqli_query($con,"select id from tblvisitor ");
+$tvisitors=mysqli_num_rows($query111);
+?>
